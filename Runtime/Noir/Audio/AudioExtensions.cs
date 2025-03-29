a0926719702f7da05f4d3248b851cf91ba6dd714
Jared Barboza <jared.m.barboza@gmail.com>
2025-03-29 10:10:49 -0400

    docs: adds code docs

--- Runtime/Noir/Audio/AudioExtensions.cs
+++ Runtime/Noir/Audio/AudioExtensions.cs
@@ -9,10 +9,20 @@ namespace Noir.Audio {
             return source.time / source.clip.length * 100f;
         }
 
+        /// <summary>
+        /// Returns a value indicating whether the <see cref="AudioSource"/> is stopped with any play time remaining.
+        /// </summary>
+        /// <param name="source">The audio source.</param>
+        /// <returns><c>true</c> if the audio source is stopped and has time remaining, <c>false</c> otherwise.</returns>
         public static bool IsAudioPaused(this AudioSource source) {
             return !source.isPlaying && GetAudioPlayedPercentage(source) > 0f;
         }
 
+        /// <summary>
+        /// Returns a value indicating whether the <see cref="AudioClip"/> is ready to be played.
+        /// </summary>
+        /// <param name="clip">The audio clip to check.</param>
+        /// <returns><c>true</c> if the clip is ready to be played, <c>false</c> otherwise.</returns>
         public static bool IsClipReadyToPlay(this AudioClip clip) {
             return clip.loadType != AudioClipLoadType.Streaming;
         }
