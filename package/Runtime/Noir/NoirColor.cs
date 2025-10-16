e43c6359cb84128beaccf8fb9af87561a292faf7
Jared Barboza <jared.m.barboza@gmail.com>
2025-10-16 08:33:39 -0400

    docs: adds doc comments to more files.

--- package/Runtime/Noir/NoirColor.cs
+++ package/Runtime/Noir/NoirColor.cs
@@ -2,7 +2,25 @@
 using UnityEngine;
 
 namespace Noir {
+    /// <summary>
+    /// Provides helper methods for creating <see cref="Color" /> objects.
+    /// </summary>
     public static class NoirColor {
+
+        /// <summary>
+        /// Creates a <see cref="Color"/> object from a hexadecimal color string.
+        /// </summary>
+        /// <param name="color">
+        /// The hexadecimal color string. Can optionally start with a '#' character.
+        /// If the string is 6 characters long (e.g., "RRGGBB"), an alpha value of "FF" (opaque) is assumed.
+        /// If the string is 8 characters long (e.g., "RRGGBBAA"), the last two characters represent the alpha channel.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Color"/> object corresponding to the specified hexadecimal color value.
+        /// </returns>
+        /// <exception cref="FormatException">
+        /// Thrown if <paramref name="color"/> is not a valid hexadecimal string.
+        /// </exception>
         public static Color FromHex(string color) {
             if (color.StartsWith("#", StringComparison.InvariantCulture)) {
                 color = color.Substring(1); // strip #
--- package/Runtime/Noir/NoirEnum.cs
+++ package/Runtime/Noir/NoirEnum.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Noir {
+    /// <summary>
+    /// Provides helper methods for interacting with <see cref="System.Enum"/> values.
+    /// </summary>
     public static class NoirEnum {
+
+        /// <summary>
+        /// Returns an enumerable collection of all possible values of the specified enum type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The enum type to retrieve values for. Must be an enumeration type.
+        /// </typeparam>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> containing all values defined in the enum <typeparamref name="T"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown if <typeparamref name="T"/> is not an enum type.
+        /// </exception>
         public static IEnumerable<T> AllValuesFor<T>() {
             return Enum.GetValues(typeof(T)).Cast<T>();
         }
